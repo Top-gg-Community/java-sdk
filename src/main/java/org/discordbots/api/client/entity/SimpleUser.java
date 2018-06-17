3b1deac8024fb5dedde2b06d2793dc3c36f06914
Gatt <zacharygatt@me.com>
2018-06-18 02:12:34 +1000

    API now returns Discrimator

--- src/main/java/org/discordbots/api/client/entity/SimpleUser.java
+++ src/main/java/org/discordbots/api/client/entity/SimpleUser.java
@@ -1,21 +1,26 @@
 package org.discordbots.api.client.entity;
 
+import java.util.StringJoiner;
+
 public class SimpleUser extends Entity {
 
     private String id;
+    private String discriminator;
     private String username;
-
     private String avatar;
 
 
-
+    public String getDiscriminator() {
+        return discriminator;
+    }
     public String getId() { return id; }
     public String getUsername() { return username; }
-
     public String getAvatar() { return avatar; }
-
-    public boolean matchesUser(String id){
+    public boolean matchesUserID(String id){
         return this.id.equalsIgnoreCase(id);
+    }
+    public boolean matchesUserName(String name, String discriminator){
+        return ((name + "#" + discriminator).equalsIgnoreCase(getUsername() + "#" + getDiscriminator()));
     }
 
 }
