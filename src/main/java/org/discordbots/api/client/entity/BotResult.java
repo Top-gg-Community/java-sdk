1d70e065cbeec73fa8c8e0faa6c6eb87c6001155
Nik <nik@monsterlyrics.co>
2018-07-14 18:24:46 -0400

    Clarified why this class is needed

--- src/main/java/org/discordbots/api/client/entity/BotResult.java
+++ src/main/java/org/discordbots/api/client/entity/BotResult.java
@@ -1,3 +1,5 @@
 package org.discordbots.api.client.entity;
 
+// This class is needed because of the way Java generics work. I can't reference Result<Bot> as a class
+// so this is just a simple workaround for that
 public class BotResult extends Result<Bot> {}
